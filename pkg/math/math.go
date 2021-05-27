3b59bd8e82ef5248c9e099db997c512f2984a9b8
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-05-27 11:55:00 +0300

    Testing
    * create test file
    	package_test.go
    * create test
    	import "testing"
    
    	TestFunc(t *testing.T) {
    		t.Error("error")
    	}
    * run test
    	go test

--- pkg/math/math.go
+++ pkg/math/math.go
@@ -2,8 +2,14 @@ package math
 
 func Average(xs []float64) float64 {
 	total := float64(0)
+
+	if len(xs) == 0 {
+		return total
+	}
+
 	for _, x := range xs {
 		total += x
 	}
+
 	return total / float64(len(xs))
 }
